4108e299d7f0f1cc89837db359d6f8cb40eebde9
EdenilsonFSantos <canalpcaomaximo@gmail.com>
2021-08-15 14:55:50 -0300

    Rename cabeçalhos
    Rename títulos, menus do cabeçalhos

--- pages/cabecalho.php
+++ pages/cabecalho.php
@@ -6,9 +6,9 @@
 
 <div class="topnav" id="myTopnav">
   <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
+  <a href="#quemsomos">Quem somos</a>
+  <a href="#contacto">Contato</a>
+  <a href="#localizacao">Localização</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
