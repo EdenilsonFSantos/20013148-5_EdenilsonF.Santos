2d51ce0f9e9820209d4df8ffab00f714c91d38ce
EdenilsonFSantos <canalpcaomaximo@gmail.com>
2021-08-15 14:42:20 -0300

    Add cabeçalho
    adicionei um modelo de cabeçalho na página, isso inclui 3 novos arquivos...
    CSS, PHP, JS

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+
+
 <?php
+	require 'pages/cabecalho.php';
+
 	echo 'Olá Mundo';
 ?>
+
+
+</html>
 
