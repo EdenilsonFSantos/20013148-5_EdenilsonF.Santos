66c1f0698b8fbaa34f52b1a3acb18170d54316ac
EdenilsonFSantos <canalpcaomaximo@gmail.com>
2021-08-22 14:51:12 -0300

    iniciado-trabalho_index
    download de images e efeitos js

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
 <html>
 <head>
 <meta content="text/html; charset=UTF-8" http-equiv="Content-Type"/>
+    <link rel="stylesheet" type="text/css" href="pages/js/Glider/glider.css">   
     <link rel="stylesheet" type="text/css" href="pages/css/style.css">
+    <script type="text/javascript" src="pages/js/Glider/glider.min.js"></script>
     <script type="text/javascript" src="pages/js/script.js"></script>
     <title>Home</title>
 </head>
 <body>
+<script src="glider.js"></script>
 
 <!-- MENUS - CABECALHO   -->
 <?php
 	require 'pages/cabecalho.php';
-
-	echo 'Olá Mundo';
 ?>
 
+<article class="c-carousel">
+  <div class="c-carousel__slides">
+    <article class="c-carousel__slide">1</article>
+    <article class="c-carousel__slide">2</article>
+    <article class="c-carousel__slide">3</article>
+    <article class="c-carousel__slide">4</article>
+    <article class="c-carousel__slide">5</article>
+    <article class="c-carousel__slide">6</article>
+  </div>
 
-
-<br>
-
-
+  <button aria-label="Anterior">«</button>
+  <button aria-label="Próximo">»</button>
+  <div role="tablist"></div>
+</article>
 
 <!-- RODAPÉ   -->
 <footer>
--- pages/js/script.js
+++ pages/js/script.js
@@ -7,3 +7,37 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  var $simpleCarousel = document.querySelector(".js-carousel--simple");
+
+  new Glider($simpleCarousel, {
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    draggable: true,
+    dots: ".js-carousel--simple-dots",
+    arrows: {
+      prev: ".js-carousel--simple-prev",
+      next: ".js-carousel--simple-next",
+    },
+  });
+  
+
+/* 
+    function slide1(){
+    document.getElementById('id').src="http://localhost/20013148-5_EdenilsonF.Santos/pages/images/doce-sagu.jpg";
+    setTimeout("slide2()", 3000)
+    document.getElementById('aId').href="http://localhost/20013148-5_EdenilsonF.Santos/pages/localizacao.php"
+    }
+    
+    function slide2(){
+    document.getElementById('id').src="http://localhost/20013148-5_EdenilsonF.Santos/pages/images/doce-de-leite-cremoso2.jpg";
+    setTimeout("slide3()", 3000)
+    document.getElementById('aId').href="link2.html"
+    }
+    
+    function slide3(){
+    document.getElementById('id').src="http://localhost/20013148-5_EdenilsonF.Santos/pages/images/goiabada_cremosa1.jpg";
+    setTimeout("slide1()", 3000)
+    document.getElementById('aId').href="link3.html"
+    }
+*/
